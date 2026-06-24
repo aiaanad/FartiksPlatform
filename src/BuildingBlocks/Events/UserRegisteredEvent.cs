0202b37bef601a917cb9cce0eececa971e28d484
kseniatolstuhina <kt26112006@mail.ru>
2026-06-24 10:35:17 +0300

    added error handling

---
+++ src/BuildingBlocks/Events/UserRegisteredEvent.cs
@@ -0,0 +1,10 @@
+namespace BuildingBlocks.Events;
+
+public record UserRegisteredEvent
+{
+    public Guid EventId { get; init; } = Guid.NewGuid();
+    public DateTime OccurredOn { get; init; } = DateTime.UtcNow;
+    public Guid PlayerId { get; init; }
+    public string Username { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+}
